3ead1e758ff35e43ae845c1f8194fffc918bd9db
Ilya Savchenko <iliya.savchenko@celadonsoft.com>
2021-10-29 23:47:18 +0300

    Updates

---
+++ precache-manifest.2318fece516e89db68149dd5aca8f601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002b4c5c05ef20e55f855a9148a66abe",
+    "url": "/Akozyrev222/homeworks/index.html"
+  },
+  {
+    "revision": "e1254fa3e1ae02aaacf7",
+    "url": "/Akozyrev222/homeworks/static/css/main.1ac6951e.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/Akozyrev222/homeworks/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Akozyrev222/homeworks/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1254fa3e1ae02aaacf7",
+    "url": "/Akozyrev222/homeworks/static/js/main.dd2900eb.chunk.js"
+  },
+  {
+    "revision": "802fb1d47076d2aedfc1",
+    "url": "/Akozyrev222/homeworks/static/js/runtime-main.f4178f71.js"
+  }
+]);
